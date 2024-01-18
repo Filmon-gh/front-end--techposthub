137a41f5e836db41591e03020cd5554e03782854
Filmon Ghebremichael <filmo2020gh@gmail.com>
2024-01-18 21:37:40 +0000

    Add routing logic with React Router  from react-bootstrap in App component.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import SignUpForm from "./pages/auth/SignUpForm"; // Import the SignUpForm component
 import "./api/axiosDefaults";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className={styles.App}>
+    <div className={styles.AppContainer}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/Log in" render={() => <h1>Log in</h1>} />
+          <Route exact path="/sign up" render={() => <h1>Sign up</h1>} />
+          <Route
+            render={() => (
+              <div>
+                <h1>404 - Page not found</h1>
+                <p>
+                  Sorry, the page you are looking for does not exist. Please
+                  check the URL or navigate to the homepage.
+                </p>
+              </div>
+            )}
+          />
+         </Switch>
+      </Container>
       <SignUpForm /> {/* Render the SignUpForm component here */}
     </div>
   );
 }
 
 export default App;
+
