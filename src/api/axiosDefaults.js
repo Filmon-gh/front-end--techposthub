e5171df2589ae5b1854683563816d72a56e6667a
Filmon Ghebremichael <filmo2020gh@gmail.com>
2024-01-28 05:52:37 +0000

    Set up Axios with default configurations for API requests.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://techposthub-frontend-1a899469cb06.herokuapp.com/";
+axios.defaults.baseURL = "https://techposthub-acad652d6abc.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
