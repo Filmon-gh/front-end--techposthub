a5e535ec6f0729304f028773b1c657cb8018b252
Filmon Ghebremichael <filmo2020gh@gmail.com>
2024-03-08 19:26:42 +0000

    Add router effect for navigation in the App component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import MainContentContainer from "react-bootstrap/Container";
 import SignUpForm from "./pages/auth/SignUpForm"; // Import the SignUpForm component
 import "./api/axiosDefaults";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
-      <SignUpForm /> {/* Render the SignUpForm component here */}
+      <MainContentContainer className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route render={() => <p>The requested page could not be found.</p>} />
+        </Switch>
+      </MainContentContainer> 
     </div>
   );
 }
