44214865481133edb3c46d4c6f68ff97ec386e85
Filmon Ghebremichael <filmo2020gh@gmail.com>
2024-03-08 19:03:04 +0000

    Add navigation links with React Router in NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>TechHubPost
-        </Navbar.Brand>
+        <Link to="/">
+          <Navbar.Brand>TechHubPost</Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-            🏠 Home
-            </Nav.Link>
-            <Nav.Link>
-            🔑 Log in
-            </Nav.Link>
-            <Nav.Link>
-            ✏️ Sign up
-            </Nav.Link>
+            <Nav.Link as={Link} to="/">🏠 Home</Nav.Link>
+            <Nav.Link as={Link} to="/signin">🔑 Log in</Nav.Link>
+            <Nav.Link as={Link} to="/signup">✏️ Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
