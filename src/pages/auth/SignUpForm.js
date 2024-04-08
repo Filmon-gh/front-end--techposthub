4ece35cfa0451f9e1d99f6467429355b88944660
Filmon Ghebremichael <filmo2020gh@gmail.com>
2024-04-08 14:50:57 +0000

    Add sign-up form inputs

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -10,10 +10,49 @@ function SignUpForm() {
   });
 
   return (
-    // Form JSX will be added in subsequent steps
     <div>
-      {/* Form content will be added here */}
+      <h2>Sign Up</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
+    
   );
 }
 
