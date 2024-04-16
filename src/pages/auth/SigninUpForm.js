fcc707e7e1c695775609556e84264c0752b548a6
Filmon Ghebremichael <filmo2020gh@gmail.com>
2024-04-16 17:56:26 +0000

    Improve error handling and code structure

---
+++ src/pages/auth/SigninUpForm.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import  "../../styles/SignInUpForm.css";
+
+function SignUpForm() {
+  
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  
+  const [errors, setErrors] = useState(null);
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", formData);
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <div className="signup-form">
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+        {errors && errors.non_field_errors?.map((message, idx) => (
+          <div key={idx} className="error-message">
+            <p>{message}</p>
+          </div>
+        ))}
+      </div>
+    </div>  
+  );
+}
+
+export default SignUpForm;
